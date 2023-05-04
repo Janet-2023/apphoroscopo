508f3f3620f333117c3e45c3d3107c3cf1436f8e
Janet <nope.janet.97@gmail.com>
2023-05-04 01:52:20 -0500

    actualizando

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ let value2 = true;
 const value10 = 0;
 value10 = 20;
 
-*/
+
 // Condicion ya mas completo
 // bebe, niño , adolescente , adulto , anciano.
 
@@ -53,7 +53,8 @@ if(age >= 0 && age < 3){
 
    dato de entrada ( dia de cumpleaños, mes de cumpleaños)
    happy path ( 09 )
-*/
+
+   */
 
 function cumpleaños(dia, mes){
     if(dia >=21 && mes==01 || dia <=19 && mes==02){
@@ -89,5 +90,86 @@ function cumpleaños(dia, mes){
     else if(dia >= 22 && mes==12 || dia <=20 && mes==01){
         console.log('Su signo es Capricornio');
     }
-
 }
+cumpleaños(24,10);
+
+
+/*
+
+
+
+const fruta = 'mandarina';
+
+switch (fruta){
+    case 'uva':
+        console.log('Es una uva');
+        break;
+
+    case 'fresa':
+        console.log('Es una fresa');
+        break;
+
+    default:
+        console.log('Es una fruta');
+}
+
+
+//Objetos
+//Arrays
+
+//objeto literal
+const person = {
+ name: 'Diego',
+ lastName: 'Cueva',
+ job: 'fronendDeveloper',
+ age : 26,
+ visa: true,
+};
+
+//console.log(person.job);
+//console.log(person['visa']);
+//console.log(person['name']);
+
+
+// Arrays
+
+const arrayNumeros = [1, 2, 3, 4, 5, 6];
+const arrayNombres = ['Diego', 'Jose', 'Manuel'];
+//console.log(arrayNumeros);
+//console.log(arrayNombres);
+
+//console.log(arrayNombres[2]);
+*/
+
+//   creando 2 objetos con 5 propiedadas cada uno.
+const myDog = {
+    name: 'Ojitos',
+    race: 'Pumi',
+    age: '8',
+    weight: '12 k',
+    size: '35 cm',
+};
+console.log(myDog['name']);
+console.log(myDog['race' ]);
+
+const myPerson = {
+    name: 'Janet',
+    lastaname: 'Ñope',
+    age: 25,
+    birthay: '24/10',
+    jobie: 'listen music',
+};
+
+console.log(myPerson.name);
+console.log(myPerson.birthay);
+
+
+// y 2 arreeglos que incluyan un arreglo de objetos y un arreglo de strings.
+
+const alumnos = ['Juan', 'Ricardo', 'Ruben', 'Derhian', 'Carlos']
+console.log(alumnos);
+
+const laptops =[{name: 'pc1', procesador: 'i7',  precio: 4000, marca: 'Lenovo'},
+   {name: 'pc2', procesador: 'i3', marca: 'hp', precio: 2500}];
+
+console.log(laptops);
